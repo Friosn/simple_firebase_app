f01af77d82868e26df687e9feff2219021ca0f5e
Friosn <franz.rios.nino@gmail.com>
2023-01-18 13:04:48 +0100

    basic and not definitive theme

---
+++ app/src/theme.jsx
@@ -0,0 +1,16 @@
+const theme = {
+  dark: {
+    primary: '#FFFFFF',
+    accent1: '#57f7E0',
+    accent2: '#22BBFF',
+    accent3: '#C400FF',
+    stats: '#FF3B4B',
+    bottons: '#EBE8E0',
+    success: '#71c55b',
+    background: '#001538',
+    failure: '#EB4C25',
+    popUpBackground: 'rgba(6, 44, 68, 0.66)',
+  },
+};
+
+export default theme;
