667858267cc5f9ab26347f00541519a120ed101f
Friosn <franz.rios.nino@gmail.com>
2023-01-19 18:35:24 +0100

    Firebase config env

---
+++ app/src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyARNLAxpBOt13T6D-O8Y8KqxrDPpzoiigM',
+  authDomain: 'learning-firebase-6b39c.firebaseapp.com',
+  projectId: 'learning-firebase-6b39c',
+  storageBucket: 'learning-firebase-6b39c.appspot.com',
+  messagingSenderId: '624551616874',
+  appId: '1:624551616874:web:27b7b49236f5bc8810c513',
+};
+
+// Initialize Firebase
+/* const app = initializeApp(firebaseConfig); */
