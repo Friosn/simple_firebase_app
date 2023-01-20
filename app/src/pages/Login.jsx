75a7bc91c18098563ba0fc514a23eb35b8e1c46f
Friosn <franz.rios.nino@gmail.com>
2023-01-20 18:03:37 +0100

    WIP login functionallity

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,10 +1,21 @@
 import { Button, FormControl, Link, TextField } from '@mui/material';
 import Box from '@mui/material/Box';
-import React from 'react';
+import React, { useState } from 'react';
 
 /* import theme from '../theme'; */
 
-const Login = () => {
+const Login = ({ onLogin }) => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    onLogin({ email, password });
+  }
+
   return (
     <Box noValidate autoComplete="off" className="login">
       <Box maxWidth="20rem">
@@ -14,13 +25,15 @@ const Login = () => {
           className="loginLogo"
         />
       </Box>
-      <FormControl className="loginForm" sx={{ margin: '1rem', gap: '1.2rem' }}>
-        <TextField required id="outlined-required" label="Email" />
-        <TextField required id="outlined-required" label="Password" />
-        <Button variant="contained" sx={{ backgroundColor: 'black' }}>
-          Login
-        </Button>
-      </FormControl>
+      <Box as="form" onSubmit={handleSubmit} marginTop={4}>
+        <FormControl className="loginForm" sx={{ margin: '1rem', gap: '1.2rem' }}>
+          <TextField type="email" required id="outlined-required" label="Email" />
+          <TextField type="password" required id="outlined-required" label="Password" />
+          <Button type="submit" variant="contained" sx={{ backgroundColor: 'black' }}>
+            {isLogin ? 'Iniciar sesión' : 'Login'}
+          </Button>
+        </FormControl>
+      </Box>
       <p>Aún no eres un usuario?</p>
       <Link href="/register">Registrate ahora</Link>
     </Box>
