c06fb43734f137007a14cff98437cfee1bb07f9e
Friosn <franz.rios.nino@gmail.com>
2023-02-02 12:48:43 +0100

    Ranking table component WIP

---
+++ app/src/components/RankingTable.jsx
@@ -0,0 +1,45 @@
+/* import { Caption, TableUI, TBody, TD, TH, THead, TR } from './UI/TableUI.jsx';
+
+const RankingTable = ({ headers, rows, caption }) => {
+ const {user} = useAuth()
+
+  return (
+    <TableUI
+      tableLayout="auto"
+      borderCollapse="collapse"
+      borderRadius="1rem"
+      backgroundColor="white"
+    >
+      <THead>
+        <TR>
+          {headers.map((header, i) => (
+            <TH key={`header${i}`} padding="1rem" fontWeight="600">
+              {header}
+            </TH>
+          ))}
+        </TR>
+      </THead>
+      <TBody>
+        {rows.map((row, i) => (
+          <TR
+            key={`row${i}`}
+            border={row[1] === user.username && `0.3rem solid ${theme.dark.accent1}`}
+          >
+            {row.map((data, i) => (
+              <TD key={`data${i}`} padding="1rem">
+                {data}
+              </TD>
+            ))}
+          </TR>
+        ))}
+      </TBody>
+      {caption && (
+        <Caption margin="1rem" color={theme.dark.primary}>
+          {caption}
+        </Caption>
+      )}
+    </TableUI>
+  );
+};
+
+export default RankingTable;*/
