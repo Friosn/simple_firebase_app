40fd871fdf353a2948829a91537d90d570d8eb7e
Friosn <franz.rios.nino@gmail.com>
2023-02-09 20:29:37 +0100

    WIP changes in register to solve authentication

--- app/src/pages/Register.jsx
+++ app/src/pages/Register.jsx
@@ -1,18 +1,23 @@
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import {
   Alert,
   Box,
   Button,
   FormControl,
+  IconButton,
+  InputAdornment,
   InputLabel,
   Link,
   MenuItem,
+  OutlinedInput,
   Select,
   TextField,
 } from '@mui/material';
+import { Container } from '@mui/system';
 import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 
 import OAuth from '../components/OAuth';
@@ -20,13 +25,20 @@ import { db } from '../firebase';
 
 const Register = () => {
   const [gender, setGender] = React.useState('');
-  const [showPass, setShowPass] = useState(false);
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
   });
   const { name, email, password } = formData;
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const navigate = useNavigate();
 
@@ -43,13 +55,13 @@ const Register = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-
+    console.log('Blabla');
     try {
       const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       updateProfile(auth.currentUser, { displayName: name });
       const user = userCredential.user;
-
+      console.log(user);
       const formDataCopy = { ...formData };
       delete formDataCopy.password;
       formDataCopy.timestamp = serverTimestamp();
@@ -62,7 +74,7 @@ const Register = () => {
   };
 
   return (
-    <Box noValidate autoComplete="off" className="login">
+    <Container noValidate autoComplete="off" className="login">
       <Box maxWidth="20rem">
         <img
           src="https://res.cloudinary.com/djaslmpgv/image/upload/v1674056580/Logos/LOGO_MADRID_DELTA_negro_nuevo_pht3cg.png"
@@ -70,49 +82,54 @@ const Register = () => {
           className="loginLogo"
         />
       </Box>
-      <FormControl
-        onSubmit={onSubmit}
-        className="loginForm"
-        sx={{ margin: '1rem', gap: '0.8rem' }}
-      >
+      <FormControl onSubmit={onSubmit} className="loginForm" sx={{ m: 1, width: '25ch' }}>
         <TextField required id="name" label="Nombre" value={name} onChange={onChange} />
-        <TextField required id="email" label="Email" value={email} onChange={onChange} />
-        <TextField
-          required
-          id="password"
-          label="Contraseña"
-          value={password}
-          onChange={onChange}
-          type={showPass ? 'text' : 'password'}
-        />
-        {showPass ? (
-          <AiFillEyeInvisible
-            onClick={() => setShowPass((previousState) => !previousState)}
-          />
-        ) : (
-          <AiFillEye onClick={() => setShowPass((previousState) => !previousState)} />
-        )}
-        <FormControl className="registerForm">
-          <InputLabel id="select-gender">Género</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="gender"
-            value={gender}
-            onChange={handleChange}
-            label="Género"
-            required
-          >
-            <MenuItem value={'Hombre'}>Hombre</MenuItem>
-            <MenuItem value={'Mujer'}>Mujer</MenuItem>
-          </Select>
-        </FormControl>
-        <Link href="/">Volver al login</Link>
-        <Button variant="contained" sx={{ backgroundColor: 'black' }}>
-          Register
-        </Button>
-        <OAuth />
       </FormControl>
-    </Box>
+      <FormControl onSubmit={onSubmit} className="loginForm" sx={{ m: 1, width: '25ch' }}>
+        <TextField required id="email" label="Email" value={email} onChange={onChange} />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: '25ch', height: '3rem' }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <InputLabel htmlFor="gender">Género</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="gender"
+          value={gender}
+          onChange={handleChange}
+          label="Género"
+          required
+          placeholder="Género"
+        >
+          <MenuItem value={'Hombre'}>Hombre</MenuItem>
+          <MenuItem value={'Mujer'}>Mujer</MenuItem>
+        </Select>
+        <Link href="/">Volver al login</Link>
+      </FormControl>
+
+      <Button variant="contained" sx={{ backgroundColor: 'black' }}>
+        Register
+      </Button>
+      <OAuth />
+    </Container>
   );
 };
 
